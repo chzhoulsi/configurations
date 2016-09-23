cf9848de09d52d7de17c92a6a996d1d9916e555e
chenzhou <chen.zhou@intel.com>
2016-09-23 13:41:15 -0500

    upload updates

--- link.sh
+++ link.sh
@@ -1,6 +1,10 @@
 file_name=`basename "$0"`
-current_path=$(readlink -f $file_name)
-
+current_path=`dirname "$0"`
+echo "file_name=$file_name current_path=$current_path"
+rm ~/.vimrc
+rm ~/.ssh
+rm ~/.tmux.conf
+rm ~/.vim -rf
 ln -s $current_path/.vimrc ~/
 ln -s $current_path/.ssh ~/
 ln -s $current_path/.tmux.conf ~/
